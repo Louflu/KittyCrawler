7e703071cdc485aa3a10af6945037ecc6c9ce98b
Simon Øygard <Simon.oygard@hotmail.com>
2026-07-28 20:16:11 +0200

    Små oppdateringer
    - Medusa deck er laget
    - "Rematch" knappen gjort om til "Main Menu". Ikke endret i kode enda.

--- kitty-crawler/GodotGame/TELT/Resources/BossData.cs
+++ kitty-crawler/GodotGame/TELT/Resources/BossData.cs
@@ -8,9 +8,9 @@ public partial class BossData : Resource
 {
     // Navn som vises f.eks "Mio"
     [Export] public string BossName { get; set; } = "";
-    //intern boss navn f.eks "mio" 
+    //intern boss navn f.eks "mio"
     [Export] public string NpcId { get; set; } = "";
-    [Export] public Array<CardData> Deck { get; set; } = new();
+    [Export] public Array<CardData> Deck { get; set; } = new(); // Hent inn tres filer, ikke lage direkte
     [Export] public CardData RewardCard { get; set; } = null;
     [Export] public Resource DialogueResource { get; set; }
 }
